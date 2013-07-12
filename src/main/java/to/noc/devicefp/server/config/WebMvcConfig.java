9df61b0e7343655f92a61b3aa0b56b20261c7262
Дмитрий Холодов <github@sec.to>
2013-07-12 10:41:30 -0500

    Imported PropertiesConfig.  Properties imported in ApplicationContextConfig were not available using @Value in controllers without it.

--- src/main/java/to/noc/devicefp/server/config/WebMvcConfig.java
+++ src/main/java/to/noc/devicefp/server/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ package to.noc.devicefp.server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -18,6 +19,7 @@ import org.springframework.web.servlet.view.JstlView;
  */
 @Configuration
 @EnableWebMvc
+@Import({PropertiesConfig.class})
 @ComponentScan(basePackages = "to.noc.devicefp.server.controller")
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
