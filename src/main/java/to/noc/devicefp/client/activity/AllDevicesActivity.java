7343204996ba912bb91e6daa505ccaac0e9e457f
Дмитрий Холодов <github@sec.to>
2013-08-24 22:40:39 -0500

    methods now handle search filter

--- src/main/java/to/noc/devicefp/client/activity/AllDevicesActivity.java
+++ src/main/java/to/noc/devicefp/client/activity/AllDevicesActivity.java
@@ -5,27 +5,45 @@ package to.noc.devicefp.client.activity;
 
 import com.google.web.bindery.requestfactory.shared.Request;
 import java.util.List;
+import to.noc.devicefp.client.entity.DeviceCs;
 import to.noc.devicefp.client.entity.DeviceProxy;
+import to.noc.devicefp.client.ui.DevicesView.FilterSubmit;
 import to.noc.devicefp.shared.AppRequestFactory;
 
 
 public class AllDevicesActivity extends AbstractDevicesActivity {
 
     private final AppRequestFactory requests;
+    private String filter = "hasJs";
+
+    private FilterSubmit filterSubmit = new FilterSubmit() {
+        @Override
+        public void applyFilter(String newFilter) {
+            AllDevicesActivity.this.filter = newFilter;
+            AllDevicesActivity.this.reloadGrid();
+        }
+    };
 
     public AllDevicesActivity(final AppRequestFactory requests) {
         super(false);
         this.requests = requests;
+        devicesView.enableSearchBox(filter, filterSubmit);
+    }
+
+    @Override
+    protected void showDetails(DeviceCs device) {
+        super.showDetails(device);
+        devicesView.enableSearchBox(filter, filterSubmit);
     }
 
     @Override
     protected Request<List<DeviceProxy>> findDevices(int firstResult, int maxResults) {
-        return requests.deviceRequest().findAllOther(firstResult, maxResults);
+        return requests.deviceRequest().findAdminView(filter, firstResult, maxResults);
     }
 
     @Override
     protected Request<Long> countDevices() {
-        return requests.deviceRequest().countAllOther();
+        return requests.deviceRequest().countAdminView(filter);
     }
 
     @Override
