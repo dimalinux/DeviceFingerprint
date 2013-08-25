1fdb8649d49bc89cb344967808d7e46316503412
Дмитрий Холодов <github@sec.to>
2013-08-24 22:43:04 -0500

    admin view methods now accept a search filter

--- src/main/java/to/noc/devicefp/server/domain/repository/DeviceRepositoryCustom.java
+++ src/main/java/to/noc/devicefp/server/domain/repository/DeviceRepositoryCustom.java
@@ -5,14 +5,19 @@ package to.noc.devicefp.server.domain.repository;
 
 import java.util.List;
 import to.noc.devicefp.server.domain.entity.Device;
+import to.noc.devicefp.server.domain.entity.OpenIdUser;
 
 public interface DeviceRepositoryCustom {
-    public List<Device> findLinkedDevices(String cookieId, int firstResult, int maxResults);
-    public List<Device> findUserDevices(long userId, int firstResult, int maxResults);
-    public List<Device> findWithIpDevices(String ipAddress, int firstResult, int maxResults);
-    public List<Device> findAllDevicesButMine(long notMineUserId, int firstResult, int maxResults);
-
     public Device findLinkedDevice(String cookieId, long deviceId);
+    public List<Device> findLinkedDevices(String cookieId, int firstResult, int maxResults);
+
     public Device findUserDevice(long userId, long deviceId);
+    public List<Device> findUserDevices(long userId, int firstResult, int maxResults);
+
     public Device findWithIpDevice(String ipAddress, long deviceId);
+    public List<Device> findWithIpDevices(String ipAddress, int firstResult, int maxResults);
+
+    long countAdminView(OpenIdUser user, String searchNarrow);
+    public List<Device> findAdminView(OpenIdUser user, String searchNarrow, int firstResult, int maxResults);
+
 }
